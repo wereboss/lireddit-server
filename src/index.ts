c6f299b833c12886107b71bd791d98f920986df1
Srinath Menon <tellsrinath@gmail.com>
2020-09-29 20:11:35 +0800

    intial commit

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,74 @@
-import 'reflect-metadata';
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import { buildSchema } from 'type-graphql';
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/Post";
+import { UserResolver } from "./resolvers/user";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { MyContext } from "./types";
 
 const main = async () => {
-    const orm = await MikroORM.init(mikroOrmConfig);
+  const orm = await MikroORM.init(mikroOrmConfig);
 
-    // const post = orm.em.create(Post,{title:'My First Post in React'});
-    await orm.getMigrator().up();
-    // console.log('------sql-----');
-    // await orm.em.persistAndFlush(post);
-    // console.log('------sql2-----');
-    // await orm.em.nativeInsert(Post,{title:'My Native post'});
-    const posts = await orm.em.find(Post,{});
-    console.log(posts);
+  // const post = orm.em.create(Post,{title:'My First Post in React'});
+  await orm.getMigrator().up();
+  // console.log('------sql-----');
+  // await orm.em.persistAndFlush(post);
+  // console.log('------sql2-----');
+  // await orm.em.nativeInsert(Post,{title:'My Native post'});
+  const posts = await orm.em.find(Post, {});
+  console.log(posts);
 
+  const app = express();
+  // app.get("/",(_,res) => {
+  //     res.send("Hello Express!!");
+  // });
 
-    const app = express();
-    // app.get("/",(_,res) => {
-    //     res.send("Hello Express!!");
-    // });
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
 
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers:[HelloResolver, PostResolver],
-            validate: false
-        }),
-        context: () => ({em: orm.em})
-    });
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, //10 yrs
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax", //csrf
+      },
+      secret: "iyubjgkuyugjvjh",
+      saveUninitialized:false,
+      resave: false,
+    })
+  );
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: ({ req, res }): MyContext => {
+        return ({ em: orm.em, req, res });
+    },
+  });
 
-    apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({ app });
 
-    app.listen(4000,() => {
-        console.log("Express started on localhost:4000");
-    });
-}
- 
+  app.listen(4000, () => {
+    console.log("Express started on localhost:4000");
+  });
+};
+
 main().catch((err) => {
-    console.error(err);
+  console.error(err);
 });
-
---
+++ src/resolvers/user.ts
@@ -0,0 +1,137 @@
+import { User } from "../entities/User";
+import { MyContext } from "../types";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+} from "type-graphql";
+import argon2 from "argon2";
+
+@InputType()
+class UserPwdInput {
+  @Field()
+  username: string;
+
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+    @Query(() => User, {nullable:true})
+    async me(
+    @Ctx() { em,req }: MyContext
+    ){
+        if(!req.session!.userId){
+            return null;
+        }
+
+        const user = await em.findOne(User,{id:req.session!.userId});
+        return user;
+    }
+
+
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg("options") options: UserPwdInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "length must be greater than 2",
+          },
+        ],
+      };
+    }
+    if (options.password.length <= 2) {
+        return {
+          errors: [
+            {
+              field: "password",
+              message: "length must be greater than 2",
+            },
+          ],
+        };
+      }
+    const hPassword = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: hPassword,
+    });
+    try{
+    await em.persistAndFlush(user);
+    } catch(err){
+        if(err.code === '23505'){
+            return {
+                errors:[{
+                    field:"username",
+                    message:"user already exists"
+                }]
+            }
+        }
+        // console.log("message:",err.message);
+    }
+    return {user};
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UserPwdInput,
+    @Ctx() { em, req }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "username not found",
+          },
+        ],
+      };
+    }
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "password not matching",
+          },
+        ],
+      };
+    }
+    
+    req.session!.userId = user.id;
+
+    return {
+      user,
+    };
+  }
+}
