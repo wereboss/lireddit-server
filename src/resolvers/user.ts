ea21d3e6f9de3b41a18f02658c93dd30dcf12c8c
Srinath Menon <tellsrinath@gmail.com>
2020-09-30 17:41:06 +0800

    updates

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -41,23 +41,20 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
-    @Query(() => User, {nullable:true})
-    async me(
-    @Ctx() { em,req }: MyContext
-    ){
-        if(!req.session!.userId){
-            return null;
-        }
-
-        const user = await em.findOne(User,{id:req.session!.userId});
-        return user;
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { em, req }: MyContext) {
+    if (!req.session!.userId) {
+      return null;
     }
 
+    const user = await em.findOne(User, { id: req.session!.userId });
+    return user;
+  }
 
   @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UserPwdInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     if (options.username.length <= 2) {
       return {
@@ -70,34 +67,39 @@ export class UserResolver {
       };
     }
     if (options.password.length <= 2) {
-        return {
-          errors: [
-            {
-              field: "password",
-              message: "length must be greater than 2",
-            },
-          ],
-        };
-      }
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "length must be greater than 2",
+          },
+        ],
+      };
+    }
     const hPassword = await argon2.hash(options.password);
     const user = em.create(User, {
       username: options.username,
       password: hPassword,
     });
-    try{
-    await em.persistAndFlush(user);
-    } catch(err){
-        if(err.code === '23505'){
-            return {
-                errors:[{
-                    field:"username",
-                    message:"user already exists"
-                }]
-            }
-        }
-        // console.log("message:",err.message);
+    try {
+      await em.persistAndFlush(user);
+    } catch (err) {
+      if (err.code === "23505") {
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "user already exists",
+            },
+          ],
+        };
+      }
+      // console.log("message:",err.message);
     }
-    return {user};
+    //log in the user by setting the cookie
+    req.session!.userId = user.id;
+
+    return { user };
   }
 
   @Mutation(() => UserResponse)
@@ -127,7 +129,7 @@ export class UserResolver {
         ],
       };
     }
-    
+
     req.session!.userId = user.id;
 
     return {
